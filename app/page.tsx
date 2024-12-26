065f699fad695a97bb2e6aeaa1480d2b0e179e7e
Lenovo <ealenxie@qq.com>
2024-12-26 11:22:15 +0800

    代码调整

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,8 @@ import ScreenShare from "@/components/ScreenShare";
 
 export default function Home() {
   return (
-    <ScreenShare />
+    <div className="flex items-center justify-center h-screen">
+      <ScreenShare />
+    </div>
   );
 }
